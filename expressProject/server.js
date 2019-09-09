20dede88e6728e1bbd718a33d9377bc05008c788
kiranFullStack <kiran.fullstack@outlook.com>
2019-09-09 14:17:03 +0530

    sending array,objects,vars as res

--- expressProject/server.js
+++ expressProject/server.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const app = express()
 
+let firstJSON = [{ name: "kiran", income: "50 Lakh" }]
+
 app.get("/", (req, res) => {
-  //   res.send("Hello motherfucker")
-  res.send({ name: "Kiran", NetWorth: "1 Crore" })
+  //   res.send({ name: "Kiran", NetWorth: "10 Crore" })
+  res.send(firstJSON)
 })
 
 app.listen(3000, () => console.log("Running on 3000"))
