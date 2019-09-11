2e08a7f0ce2f1119fb2e57976aa9ccb6df6b4a64
kiranFullStack <kiran.fullstack@outlook.com>
2019-09-12 01:37:25 +0530

    npm init esm

---
+++ ModernNode/main.js
@@ -0,0 +1,12 @@
+// ESM syntax is supported.
+import express from "express"
+
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express 🚀 🚀 🚀 🚀 🚀 🚀")
+})
+
+app.listen(5000, () => {
+  console.log("Server running on 5000 🚀 🚀 🚀 🚀 🚀 🚀")
+})
