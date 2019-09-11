74adbaf53ef2f53fe0718ea723c4c49d4c7c7c50
kiranFullStack <kiran.fullstack@outlook.com>
2019-09-12 02:02:44 +0530

    Routes in router.js

--- ModernNode/main.js
+++ ModernNode/main.js
@@ -1,11 +1,10 @@
 // ESM syntax is supported.
 import express from "express"
+import router from "./routes"
 
 const app = express()
 
-app.get("/", (req, res) => {
-  res.send("Hello from Express 🚀 🚀 🚀 🚀 🚀 🚀")
-})
+app.use("/", router)
 
 app.listen(5000, () => {
   console.log("Server running on 5000 🚀 🚀 🚀 🚀 🚀 🚀")
